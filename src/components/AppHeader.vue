2aff2ca30b307a9cbdcac2b105e4fb61f6b9b79e
AdelMysterio <storliniadelmo@gmail.com>
2024-06-15 11:54:41 +0200

    moved appsearch in header component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,64 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
+import SearchBar from './SearchBar.vue';
+
 export default {
+
+    components: {
+        SearchBar,
+    },
+
     data() {
         return {
-
+            store,
         }
-    }
+    },
+
+    methods: {
+        searchMovie(movieName) {
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: movieName
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.store.movies = response.data.results
+                    this.searchSeries(movieName)
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+
+        searchSeries(seriesName) {
+            axios.get('https://api.themoviedb.org/3/search/tv', {
+                params: {
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: seriesName
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.store.series = response.data.results
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+
+
+    },
 }
+
+
 </script>
 
 <template>
-<h1>header</h1>
+    <h1>header</h1>
+    <SearchBar @movieSearch="searchMovie" />
 </template>
 
 <style scoped></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,10 @@
 <script>
 import { store } from '../store.js';
-import axios from 'axios';
-import SearchBar from './SearchBar.vue';
 import MovieList from './MovieList.vue';
 import SeriesList from './SeriesList.vue';
 
 export default {
-
     components: {
-        SearchBar,
         MovieList,
         SeriesList
     },
@@ -19,52 +15,11 @@ export default {
         }
     },
 
-    methods: {
-        searchMovie(movieName) {
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: 'e99307154c6dfb0b4750f6603256716d',
-                    query: movieName
-                }
-            })
-                .then((response) => {
-                    console.log(response.data.results);
-                    this.store.movies = response.data.results
-                    this.searchSeries(movieName)
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        },
-
-        searchSeries(seriesName) {
-            axios.get('https://api.themoviedb.org/3/search/tv', {
-                params: {
-                    api_key: 'e99307154c6dfb0b4750f6603256716d',
-                    query: seriesName
-                }
-            })
-                .then((response) => {
-                    console.log(response.data.results);
-                    this.store.series = response.data.results
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        },
-
-
-    },
-
-    created() {
-
-    }
 }
 </script>
 
 <template>
     <main>
-        <SearchBar @movieSearch="searchMovie" />
         <h1>Film</h1>
         <MovieList />
         <h1>Serie TV</h1>
